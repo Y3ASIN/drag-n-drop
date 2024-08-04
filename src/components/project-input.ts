5b0afb428d901937661b3dafaa560e2c19dddd6d
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-08-05 00:45:23 +0600

    Update folder structure.

---
+++ src/components/project-input.ts
@@ -0,0 +1,82 @@
+import { Component } from "./base-component.js";
+import { Validation, validate } from "../utils/validation.js";
+import { autobind } from "../decorators/autobind.js";
+import { projectState } from "../states/project-state.js";
+
+// ProjectInput class
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleElement: HTMLInputElement;
+  descriptionElement: HTMLInputElement;
+  peopleElement: HTMLInputElement;
+
+  constructor() {
+    super("project-input", "app", true, "user-input");
+
+    this.titleElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+  }
+
+  configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+  renderContent(): void {}
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleElement.value;
+    const enteredDescription = this.descriptionElement.value;
+    const enteredPeople = this.peopleElement.value;
+
+    const titleValidatable: Validation = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validation = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validation = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleElement.value = "";
+    this.descriptionElement.value = "";
+    this.peopleElement.value = "";
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      projectState.addProject(title, desc, people);
+      this.clearInputs();
+    }
+  }
+}
---
+++ src/decorators/autobind.ts
@@ -0,0 +1,16 @@
+// autobind decorator
+export function autobind(
+  _targe: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const bindFn = originalMethod.bind(this);
+      return bindFn;
+    },
+  };
+  return adjDescriptor;
+}
